098a2a18751c4dd6ff8d85cd0fe13667e6321ab3
hael-mou <hamzaelmoudden2@gmail.com>
2023-06-04 13:46:36 +0100

    new struct for tokenizer

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hael-mou <hael-mou@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 14:35:28 by hael-mou          #+#    #+#             */
-/*   Updated: 2023/06/03 16:48:33 by hael-mou         ###   ########.fr       */
+/*   Updated: 2023/06/04 10:48:23 by hael-mou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,7 @@
 /*###############################################################
 #		* Includes :		    							 	#
 ###############################################################*/
-# include <stdlib.h>
-# include <fcntl.h>
-# include <stdio.h>
-# include "defines.h"
-# include "libft.h"
-# include "minishell_types.h"
 
-// global var :
-t_list  *env;
-
-// ACTIVE BONUS
-# ifndef BONUS
-#  define BONUS 0
-# endif
 
 /*###############################################################
 #		*  DEFINES :											#
@@ -37,18 +24,11 @@ t_list  *env;
 // SHELL INFO :
 # define PROGRAM_INFO   "Minishell 2023 [Version 1.0.0.0]."
 # define DEVLOPERS  "Copyright © OEZZAOU && HAEL-MOU. All Rights Reserved."
+
 /*###############################################################
 #		* Functions :											#
 ###############################################################*/
-// builtins :
-void	shell_env(void);
-void	export_show(void);
-int 	shell_export(char *new_var);
-
-// var utils :
-t_var   *new_var(char *key, char *value);
-t_list  *get_var(char *key);
-char	*extract_key(char *env_var);
-char	*extract_value(char *env_var);
+// minishell toknizer
+t_list	*tokenizer(char *line);
 
 #endif
